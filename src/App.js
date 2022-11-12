ab75203df32d4b93a36e4c3a3aea3d9b135255e0
muhammedirfan <irfannadapuram>
2022-11-12 07:27:09 +0530

    added navbar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import Navbar from './Components/NavBar/NavBar.js'; 
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <Navbar/>
        
        
       </header>
