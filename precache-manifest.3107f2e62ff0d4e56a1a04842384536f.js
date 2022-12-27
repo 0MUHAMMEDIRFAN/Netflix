68a0e892f1381499169f52bc49b92f5ff33118e0
muhammedirfan <irfannadapuram>
2022-12-27 15:45:15 +0530

    Updates

---
+++ precache-manifest.3107f2e62ff0d4e56a1a04842384536f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "43d71c2269c63048eecb",
+    "url": "/Netflix/static/css/main.f4fbf188.chunk.css"
+  },
+  {
+    "revision": "43d71c2269c63048eecb",
+    "url": "/Netflix/static/js/main.a4c5ef17.chunk.js"
+  },
+  {
+    "revision": "403c434e8f272edbb673",
+    "url": "/Netflix/static/js/runtime~main.c07c80b2.js"
+  },
+  {
+    "revision": "59bb1f09819d0410d928",
+    "url": "/Netflix/static/js/2.e6c96725.chunk.js"
+  },
+  {
+    "revision": "335c43b975678e19734a604e043e545a",
+    "url": "/Netflix/index.html"
+  }
+];
